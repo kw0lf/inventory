8b96617fc65a51cb15f0d80a602c99719de246c5
paromita92 <pgorai@kreeti.com>
2017-04-18 16:56:00 +0530

    [#323] created brand category and item

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe Brand, type: :model do
   describe "#provides category wise item count" do
     let!(:category) { create(:category) }
     let!(:brand) { create(:brand) }
-    let!(:item) { create(:item, category: category1, brand: brand1) }
+    let!(:item) { create(:item, category: category, brand: brand) }
 
     context "the number of item's for different brand categorie's"
       it "should return one item for a category of brand " do
-        expect(brand1.category_wise_items_count).to eq("#{category1.name}-1")
+        expect(brand.category_wise_items_count).to eq("#{category.name}-1")
       end
 
     context "no item's for different brand categorie's"
       it "should not return anything" do
-        expect(brand1.category_wise_items_count).to be_nil
+        expect(brand.category_wise_items_count).to be_nil
     end
   end
 end
