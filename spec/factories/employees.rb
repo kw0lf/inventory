24dedad68a948515a07060f9e0060b4edaac2dac
paromita92 <pgorai@kreeti.com>
2017-05-23 12:07:16 +0530

    fixed indentation

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -2,8 +2,8 @@ require 'faker'
 
 FactoryGirl.define do
   factory :employee do |f|
-    f.name { Faker::Name.name }
-    f.email{ Faker::Internet.email }
+    f.name  { Faker::Name.name }
+    f.email { Faker::Internet.email }
     active "true"
   end
 end
